8db9d5339b4eb609d902baa1eb49549908b8d2cb
Officialravi888 <youremail@yourdomain.com>
2023-01-25 16:34:20 +0530

    Initial commit

---
+++ src/main/java/org/example/Cloud_Server_SpringBoot_pro/CloudServerSpringBootProApplication.java
@@ -0,0 +1,13 @@
+package org.example.Cloud_Server_SpringBoot_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerSpringBootProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerSpringBootProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/Cloud_Server_SpringBoot_pro/CloudServerSpringBootProApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.Cloud_Server_SpringBoot_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerSpringBootProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
